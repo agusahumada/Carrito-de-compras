07cbcde1e5b77882bfa06517b3c2519b4670e72d
agusahumada <agustinaahumada649@gmail.com>
2020-07-19 23:24:28 -0300

    Agregando al carrito los cursos seleccionados

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 //Variables
+
 const carrito = document.getElementById("carrito");
-const listaCursos = document.getElementById("lista-cursos");
+const cursos = document.getElementById("lista-cursos");
+const listaCursos = document.querySelector("#lista-carrito tbody");
 
 //Event Listeners
+
 cargarEventListeners();
 
 function cargarEventListeners() {
-  //Dispara cuando se presiona agregar carrito
-  listaCursos.addEventListener("click", comprarCurso);
+  //Cuando se presiona agregar carrito
+  cursos.addEventListener("click", comprarCurso);
 }
 
 //Funciones
+
 //funcion que añade el curso al carrito
 function comprarCurso(e) {
   e.preventDefault();
@@ -22,7 +26,32 @@ function comprarCurso(e) {
     leerDatosCurso(curso);
   }
 }
+
 //Lee los  datos del curso
 function leerDatosCurso(curso) {
-  console.log(curso);
+  const infoCurso = {
+    imagen: curso.querySelector("img").src,
+    titulo: curso.querySelector("h4").textContent,
+    precio: curso.querySelector(".precio span").textContent,
+    id: curso.querySelector("a").getAttribute("data-id"),
+  };
+
+  insertarCarrito(infoCurso);
+}
+
+//Muestra curso seleccionado en el carrito
+
+function insertarCarrito(curso) {
+  const row = document.createElement("tr");
+  row.innerHTML = `
+  <td>
+    <img src = "${curso.imagen}" width=150>
+  </td>
+  <td>${curso.titulo}</td>
+  <td>${curso.precio}</td>
+  <td>
+  <a href="#" class="borrar-curso data-id="${curso.id}" >X<</a>
+  </td>
+  `;
+  listaCursos.appendChild(row);
 }
